942fc31034c59529a3c7cec5e9f302e7e46a74ec
k1888763 <k1888763@kcl.ac.uk>
2020-02-12 13:29:27 +0000

    Implement MedicineManager Interface

---
+++ back-end/src/main/java/com/projectBackEnd/MedicineManagerInterface.java
@@ -0,0 +1,20 @@
+package main.java.com.projectBackEnd;
+
+import org.hibernate.SessionFactory;
+import java.util.List;
+
+public interface MedicineManagerInterface {
+
+    Medicine createMedicine(String name, String type);
+    void createAndSaveMedicine(String name, String type);
+    Medicine findByID(Integer id);
+    void updateMedicine(Medicine medicine);
+    void updateNameByID(Integer id, String name);
+    void updateTypeByID(Integer id, String type);
+    void deleteByPrimaryKey(Integer id);
+    SessionFactory getSessionFactory();
+    List<Medicine> getAll();
+    void deleteAll();
+    void insertTuple(Medicine medicine);
+
+}
