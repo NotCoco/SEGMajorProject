d5867843d9968321c577bd33509edd8236b8990b
Wasif <wasifofo@yahoo.co.uk>
2020-02-20 00:11:06 +0000

    Remove unnecessary files

--- back-end/src/main/java/com/projectBackEnd/RestInterface.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.projectBackEnd;
-
-/**
- * Each Interface which accesses the database that will be required by the REST API endpoints
- * will be extended here so all the methods are in one place.
- */
-public interface RestInterface extends PageRestInterface { //extends MedicineRestInterface etc.
-}
