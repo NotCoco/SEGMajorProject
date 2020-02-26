efac9cb6c525a243dad3bb14d0b200b39efc7df1
Andy <andrea.ciuffo@kcl.ac.uk>
2020-02-26 12:27:20 +0000

    Move styling to styles.scss

--- prototypes/proto-base/src/App.vue
+++ prototypes/proto-base/src/App.vue
@@ -5,24 +5,8 @@
 </template>
 
 <style lang="scss">
-
-@import 'bulma';
-@import 'global-styles';
-
-
+@import 'styles';
 #app {
-  // font-family: Avenir, Helvetica, Arial, sans-serif;
-  font-family: 'Open Sans', Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-
   height: 100%;
 }
-
-html, body {
-  height: 100%;
-  overflow: auto;
-}
-
 </style>
