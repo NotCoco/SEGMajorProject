4d02e2b62190735e371a5465d589ecbfcf1aef99
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-11 23:10:25 +0000

    Clean up Session package

--- back-end/src/main/java/com/projectBackEnd/Entities/Session/Session.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Session/Session.java
@@ -13,47 +13,60 @@ import java.sql.Timestamp;
 import java.io.Serializable;
 @Entity
 @Table(name = Session.TABLENAME)
-public class Session implements TableEntity{
+public class Session implements TableEntity {
+
+	// Database table columns
 	public final static String TABLENAME = "Sessions";
 	private final static String TOKEN = "Token";
 	private final static String DATE = "Date";
 	private final static String TIMEOUT = "Timeout";
 	private final static String USERNAME = "Username";
+
+	// Private fields
 	@Id
-    	@Column(name = TOKEN)
+	@Column(name = TOKEN)
 	private String token;
+
 	@Column(name = DATE)
 	private Timestamp date;
+
 	@Column(name = TIMEOUT)
 	private Timestamp timeout;
+
 	@Column(name = USERNAME)
 	private String username;
+
+
 	/**
 	*	timeout in sec
 	*/
-	public Session(String username, int timeout){
+	public Session(String username, int timeout) {
 		this.username = username;
 		this.date = new Timestamp(System.currentTimeMillis());
 		this.timeout = new Timestamp(System.currentTimeMillis() + timeout * 1000);
 		this.token = generateToken();
 	}
+
+
 	public Session(){};
-    	public Serializable getPrimaryKey(){
+
+	public Serializable getPrimaryKey(){
 		return token;
 	}
-	private String generateToken(){
+
+	private String generateToken() {
+
 		Random rand = new Random();
 		String s = null;
-        	String alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz"; 		
-		do{
+		String alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+		do {
 			StringBuilder sb = new StringBuilder();		
-			for(int i = 0; i < 26;++i){
-				sb.append(alphaNum.charAt(rand.nextInt(alphaNum.length())));
-			}
+			for(int i = 0; i < 26;++i) sb.append(alphaNum.charAt(rand.nextInt(alphaNum.length())));
 			s = sb.toString();
-		}while(s == null && SessionManager.getSessionManager().verifySession(s));	
+		} while(s == null && SessionManager.getSessionManager().verifySession(s));
 		return s;
 	}
+
 	public String getToken(){
 		return token;	
 	}
@@ -67,19 +80,17 @@ public class Session implements TableEntity{
 		return username;
 	}
 
-    	public TableEntity copy(TableEntity newCopy){
-		if(newCopy instanceof Session){		
+
+	public TableEntity copy(TableEntity newCopy) {
+		if (newCopy instanceof Session) {
 			token = ((Session)newCopy).getToken();
 			date = ((Session)newCopy).getDate();
 			timeout = ((Session)newCopy).getTimeout();
 			username = ((Session)newCopy).getUsername();
 			return this;
 		}
-		else
-			return null;
-		
+		else return null;
 	}
-	
 
 	
 } 
--- back-end/src/main/java/com/projectBackEnd/Entities/Session/SessionManager.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Session/SessionManager.java
@@ -5,45 +5,63 @@ import main.java.com.projectBackEnd.HibernateUtility;
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.Timestamp;
-public class SessionManager extends EntityManager implements SessionManagerInterface{
+
+/**
+ * SessionManager defines methods for Session objects to interact with the database.
+ * This class extends the EntityManager.
+ *
+ * Inspired by :
+ * https://examples.javacodegeeks.com/enterprise-java/hibernate/hibernate-annotations-example/
+ */
+public class SessionManager extends EntityManager implements SessionManagerInterface {
+
 	private static SessionManagerInterface sessionManager;
-	private SessionManager(){
-	        super();
-        	setSubclass(Session.class);
-        	HibernateUtility.addAnnotation(Session.class);
+
+	private SessionManager() {
+		super();
+		setSubclass(Session.class);
+		HibernateUtility.addAnnotation(Session.class);
 		sessionManager = this;
 	}
-	public static SessionManagerInterface getSessionManager(){
-		if(sessionManager != null)
-			return sessionManager;
-		else
-			return new SessionManager();
+
+	/**
+	 * @return sessionManager ; if none has been defined, create a new SessionManager
+	 */
+	public static SessionManagerInterface getSessionManager() {
+		if(sessionManager != null) return sessionManager;
+		else return new SessionManager();
 	}
-	public boolean verifySession(String token){
+
+
+	public boolean verifySession(String token) {
+
 		List<Session> sessions = getAll();
 		List<Session> correct = new ArrayList<Session>();
-		for(Session s: sessions){
-			if(s.getToken().equals(token))
-				correct.add(s);
+
+		for (Session s: sessions) {
+			if(s.getToken().equals(token)) correct.add(s);
 		}		
 	
-		if(correct.size() == 1){
+		if (correct.size() == 1) {
 			Session current = correct.get(0);
 			Timestamp now = new Timestamp(System.currentTimeMillis());
-			if(current.getTimeout().after(now))
-				return true;
+			if(current.getTimeout().after(now)) return true;
 			delete(current);		
 		}
+
 		return false;
 	}
-	public String getNewSession(String username,int timeout){
+
+
+	public String getNewSession(String username,int timeout) {
 		Session s = new Session(username,timeout);
 		insertTuple(s);
 		return s.getToken();
 	}
-	public void terminateSession(String token){
-		if(getAll().stream().filter(s->((Session)s).getToken().equals(token)).count() == 1)
-		{
+
+
+	public void terminateSession(String token) {
+		if(getAll().stream().filter(s->((Session)s).getToken().equals(token)).count() == 1) {
 			Session s = (Session)getByPrimaryKey(token);
 			delete(s);	
 		}	
