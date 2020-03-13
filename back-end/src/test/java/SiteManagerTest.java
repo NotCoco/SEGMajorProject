3b0233a2fbb29025099f35f5b0b3ff429e63b925
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-13 00:11:52 +0000

    Update SiteManagerTest.java

--- back-end/src/test/java/SiteManagerTest.java
+++ back-end/src/test/java/SiteManagerTest.java
@@ -1,9 +1,16 @@
 package test.java;
 
+import main.java.com.projectBackEnd.Entities.Site.Site;
 import main.java.com.projectBackEnd.Entities.Site.SiteManager;
 import main.java.com.projectBackEnd.Entities.Site.SiteManagerInterface;
 import main.java.com.projectBackEnd.HibernateUtility;
 import org.junit.*;
+
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class SiteManagerTest {
 
@@ -28,5 +35,69 @@ public class SiteManagerTest {
         siteManager.deleteAll();
     }
 
+    @Test
+    public void testCreateSite() {
+        Site site = new Site("Biliary Atresia");
+        assertEquals(site.getName(), "Biliary Atresia");
+    }
+
+    @Test
+    public void testCreateAndSaveSite() {
+        siteManager.addSite("Biliary Atresia");
+        assertEquals(siteManager.getAllSites().size(), 1);
+    }
+
+    @Test
+    public void testCreateIllegalSite() {
+        siteManager.addSite(new Site(null, null));
+        assertEquals(siteManager.getAllSites().size(), 0);
+    }
+
+    @Test
+    public void testFillingAndGettingSites() {
+        fillDatabase();
+        assertEquals(siteManager.getAllSites().size(), 8);
+    }
+
+    @Test
+    public void testDuplicateName() {
+        siteManager.addSite("Biliary Atresia");
+        siteManager.addSite("Biliary Atresia");
+        assertEquals(1, siteManager.getAllSites().size());
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testUpdateWithIllegalValues() {
+        fillDatabase();
+        Site site = new Site(siteManager.getAllSites().get(0).getPrimaryKey(), null);
+        siteManager.update(site);
+    }
+
+    
+    /**
+     * @return array list of example site objects for database filling
+     */
+    private static ArrayList<Site> getListOfSites() {
+
+        ArrayList<Site> listOfSites = new ArrayList<>();
+
+        listOfSites.add(new Site("Disease1"));
+        listOfSites.add(new Site("Disease2"));
+        listOfSites.add(new Site("Disease3"));
+        listOfSites.add(new Site("Disease4"));
+        listOfSites.add(new Site("Disease5"));
+        listOfSites.add(new Site("Disease6"));
+        listOfSites.add(new Site("Disease7"));
+        listOfSites.add(new Site("Disease8"));
+
+        return listOfSites;
+    }
+
+    /**
+     * Add sites to database
+     */
+    private void fillDatabase() {
+        for (Site site : getListOfSites()) siteManager.addSite(site);
+    }
 
 }
