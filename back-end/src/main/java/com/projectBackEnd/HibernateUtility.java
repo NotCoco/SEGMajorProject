a72b22281cdccee546b17eb8d2ea9ffade4540eb
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-17 17:10:53 +0000

    Comment EntityManager

--- back-end/src/main/java/com/projectBackEnd/HibernateUtility.java
+++ back-end/src/main/java/com/projectBackEnd/HibernateUtility.java
@@ -8,20 +8,19 @@ import java.util.HashSet;
 /**
  * http://www.jcombat.com/hibernate/introduction-to-hibernateutil-and-the-sessionfactory-interface
  */
-public class HibernateUtility
-{
+public class HibernateUtility {
+
     private static SessionFactory sessionFactory;
     private static String resourceName="hibernate.cfg.xml";
     private static HashSet<Class> annotations;
 
-    public synchronized static SessionFactory buildSessionFactory()
-    {
+    public synchronized static SessionFactory buildSessionFactory() {
         if (sessionFactory != null) {
             if (sessionFactory.isOpen()) sessionFactory.close();
         }
         try {
             Configuration cfg = new Configuration();
-            for(Class a : annotations){
+            for(Class a : annotations) {
                 cfg.addAnnotatedClass(a);
             }
             sessionFactory = cfg.configure(resourceName).buildSessionFactory();
@@ -37,10 +36,8 @@ public class HibernateUtility
         resourceName = newName;
     }
 
-    public static void addAnnotation(Class c){
-        if (annotations == null) {
-            annotations = new HashSet<>();
-        }
+    public static void addAnnotation(Class c) {
+        if (annotations == null) annotations = new HashSet<>();
         if (annotations.contains(c)) return;
         annotations.add(c);
         buildSessionFactory();
@@ -48,15 +45,11 @@ public class HibernateUtility
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory != null && !sessionFactory.isClosed()) return sessionFactory;
-        else {
-            return buildSessionFactory();
-        }
+        else return buildSessionFactory();
     }
 
     public static void shutdown() {
         getSessionFactory().close();
     }
-
-    //TODO Allow dynamic controlling location of DB and class
 
 }
--- back-end/src/main/java/com/projectBackEnd/TableEntity.java
+++ back-end/src/main/java/com/projectBackEnd/TableEntity.java
@@ -2,7 +2,9 @@ package main.java.com.projectBackEnd;
 
 import java.io.Serializable;
 
-
+/**
+ * Interface implemented by all database entity tables
+ */
 public interface TableEntity {
     Serializable getPrimaryKey();
     TableEntity copy(TableEntity newCopy);
