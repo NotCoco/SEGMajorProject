9cb86e6039d9fd6d9d4b8aba890c5f866aa5b6b6
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-17 19:48:06 +0000

    Change variable names in NewsController

--- back-end/src/main/java/com/projectBackEnd/Entities/News/NewsController.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/News/NewsController.java
@@ -32,22 +32,22 @@ public class NewsController {
     @Post("/")
     public HttpResponse<News> add(@Body NewsAddCommand command) {
 
-        News n = newsManager.addNews(command.getDate(), command.isPinned(), command.getDescription(),
+        News news = newsManager.addNews(command.getDate(), command.isPinned(), command.getDescription(),
                 command.getTitle(), command.isUrgent(), command.getContent(), command.getSlug());
 
-        if(newsManager.getByPrimaryKey(n.getPrimaryKey()) == null)return HttpResponse.serverError();
+        if(newsManager.getByPrimaryKey(news.getPrimaryKey()) == null)return HttpResponse.serverError();
 
         return HttpResponse
-                .created(n)
-                .headers(headers -> headers.location(location(n.getPrimaryKey())));
+                .created(news)
+                .headers(headers -> headers.location(location(news.getPrimaryKey())));
     }
 
 
     @Put("/")
     public HttpResponse update(@Body NewsUpdateCommand command) {
-        News n = new News(command.getId(), command.getDate(), command.isPinned(), command.getDescription(),
+        News news= new News(command.getId(), command.getDate(), command.isPinned(), command.getDescription(),
                 command.getTitle(), command.isUrgent(), command.getContent(), command.getSlug());
-        newsManager.update(n);
+        newsManager.update(news);
 
         return HttpResponse
                 .noContent()
