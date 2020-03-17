1c80cf57625d5c79e6bb0c26237e75e3e549cdfc
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-17 22:02:19 +0000

    Remove unecessary methods from MedicineInterface and Manager

--- back-end/src/main/java/com/projectBackEnd/Entities/Medicine/MedicineManagerInterface.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Medicine/MedicineManagerInterface.java
@@ -14,10 +14,7 @@ public interface MedicineManagerInterface {
     public Medicine update(Medicine med);
     public Medicine getByPrimaryKey(Serializable id);
     public List<Medicine> getAllMedicines();
-    public List<Medicine> getAllMedicinesByType(String type);
-    public List<Medicine> getAllMedicinesByName(String name);
     public void deleteAll();
-    public void delete(Medicine med);
     public void delete(Serializable pk);
 
 }
