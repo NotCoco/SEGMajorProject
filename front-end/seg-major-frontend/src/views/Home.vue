0ed3dd975f467b348d735e450d7277e3b6bcde17
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-17 17:16:06 +0000

    Create frontend project

---
+++ front-end/seg-major-frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home">
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Paediatric Liver Service</h1>
+          <h2 class="subtitle">King's College Hospital</h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
