ddc095b0108b208d8bd822345bf817b536ee6e18
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-03-17 18:37:24 +0000

    Refactor MedicineUpdateCommand

--- back-end/src/main/java/com/projectBackEnd/Entities/Medicine/MedicineUpdateCommand.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Medicine/MedicineUpdateCommand.java
@@ -1,52 +1,24 @@
 package main.java.com.projectBackEnd.Entities.Medicine;
-import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.NotNull;
-//import io.micronaut.core.annotation.Introspected;
 
 /**
  * MedicineUpdateCommand is an implementation of the Command design pattern.
  * It creates mock medicine objects and reduced memory use.
  * It is used by the controller to update a medicine object.
  */
-//TODO Reinsert Tags as I couldn't get them to compile on IntelliJ
-//@Introspected
-public class MedicineUpdateCommand {
+public class MedicineUpdateCommand extends MedicineAddCommand {
 
+    @NotNull
     private int id;
-    @NotNull
-    private String name;
-    @NotNull
-    private String type;
 
-    public MedicineUpdateCommand() {};
+    public MedicineUpdateCommand() {super();}
 
     public MedicineUpdateCommand(int id, String name, String type) {
+        super(name, type);
         this.id = id;
-        this.name = (name != null && "".equals(name.trim())) ? "Unnamed" : name;
-        this.type = (type != null && "".equals(type.trim())) ? "Undefined" : type;
     }
-
-    /**
-     * Getters and setters
-     */
 
     public int getId() { return id; }
 
-    public String getName(){ return name; }
-
-    public String getType(){
-        return type;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = (name != null && "".equals(name.trim())) ? "Unnamed" : name;
-    }
-
-    public void setType(String type) {
-        this.type = (type != null && "".equals(type.trim())) ? "Undefined" : type;
-    }
 }
