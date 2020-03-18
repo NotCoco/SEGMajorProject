0b0a469db94f0fe2356df45ba5145b97ba71280b
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-18 18:20:41 +0000

    Remove semicolon

--- front-end/seg-major-frontend/src/services/sites-service.js
+++ front-end/seg-major-frontend/src/services/sites-service.js
@@ -3,6 +3,6 @@ import BackendService from './backend-service'
 export default {
     async createSite(siteName) {
         const res = await BackendService.createSite(siteName)
-        return res.data;
+        return res.data
     }
 }
