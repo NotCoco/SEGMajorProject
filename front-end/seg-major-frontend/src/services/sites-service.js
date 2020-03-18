5653e2bc0b68e41634c8612f4a9071af0bbb2399
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-18 18:21:30 +0000

    Implement getAllSites method in sites-service

--- front-end/seg-major-frontend/src/services/sites-service.js
+++ front-end/seg-major-frontend/src/services/sites-service.js
@@ -4,5 +4,10 @@ export default {
     async createSite(siteName) {
         const res = await BackendService.createSite(siteName)
         return res.data
+    },
+
+    async getAllSites() {
+        const res = await BackendService.getAllSites()
+        return res.data
     }
 }
