e3a72a61c255d8dc25a3f2b6cfc58fd5c76f8cc6
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-18 18:17:20 +0000

    Implement createSite method in sites-service

--- front-end/seg-major-frontend/src/services/sites-service.js
+++ front-end/seg-major-frontend/src/services/sites-service.js
@@ -1,5 +1,8 @@
 import BackendService from './backend-service'
 
 export default {
-    
+    async createSite(siteName) {
+        const res = await BackendService.createSite(siteName)
+        return res.data;
+    }
 }
