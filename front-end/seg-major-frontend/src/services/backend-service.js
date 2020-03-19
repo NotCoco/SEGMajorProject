625cb61484573eca8ce98a9dc1724703fc6abdc3
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-19 18:00:10 +0000

    Change formatting of backend-service so it uses 2 spaces for indents

--- front-end/seg-major-frontend/src/services/backend-service.js
+++ front-end/seg-major-frontend/src/services/backend-service.js
@@ -1,49 +1,49 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+  baseURL: 'http://localhost:8080'
 })
 
 export default {
-    createSite(siteName) {
-        return api.post('/sites', { name: siteName })
-    },
+  createSite(siteName) {
+    return api.post('/sites', { name: siteName })
+  },
 
-    getAllSites() {
-        return api.get('/sites')
-    },
+  getAllSites() {
+    return api.get('/sites')
+  },
 
-    getSite(siteName) {
-        return api.get(`/sites/${siteName}`)
-    },
+  getSite(siteName) {
+    return api.get(`/sites/${siteName}`)
+  },
 
-    getAllPages(siteName) {
-        return api.get(`/sites/${siteName}/pages`)
-    },
+  getAllPages(siteName) {
+    return api.get(`/sites/${siteName}/pages`)
+  },
 
-    getPage(siteName, pageSlug) {
-        return api.get(`/sites/${siteName}/pages/${pageSlug}`)
-    },
+  getPage(siteName, pageSlug) {
+    return api.get(`/sites/${siteName}/pages/${pageSlug}`)
+  },
 
-    createPage(page) {
-        console.log(page)
-        return api.post(`/sites/${page.site.name}/pages`, {
-            "site": page.site.name,
-            "slug": page.slug,
-            "index": page.index,
-            "title": page.title,
-            "content": page.content
-        })
-    },
+  createPage(page) {
+    console.log(page)
+    return api.post(`/sites/${page.site.name}/pages`, {
+      "site": page.site.name,
+      "slug": page.slug,
+      "index": page.index,
+      "title": page.title,
+      "content": page.content
+    })
+  },
 
-    updatePage(page) {
-        return api.put(`/sites/${page.site.name}/pages`, {
-            "primaryKey": page.primaryKey,
-            "site": page.site,
-            "slug": page.slug,
-            "index": page.index,
-            "title": page.title,
-            "content": page.content
-        })
-    }
+  updatePage(page) {
+    return api.put(`/sites/${page.site.name}/pages`, {
+      "primaryKey": page.primaryKey,
+      "site": page.site,
+      "slug": page.slug,
+      "index": page.index,
+      "title": page.title,
+      "content": page.content
+    })
+  }
 }
