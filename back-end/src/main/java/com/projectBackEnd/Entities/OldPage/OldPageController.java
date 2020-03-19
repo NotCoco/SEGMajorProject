f9114c58ccb86ccbc3ebcb9d864c1ce89afe1c31
Wasif <wasifofo@yahoo.co.uk>
2020-03-19 18:20:17 +0000

    Repair PageManager getters

--- back-end/src/main/java/com/projectBackEnd/Entities/OldPage/OldPageController.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/OldPage/OldPageController.java
@@ -10,62 +10,6 @@ import java.net.URLEncoder;
 import java.net.URI;
 import java.util.List;
 
-@Controller("/page")
 public class OldPageController {
-    protected final OldPageManagerInterface pageManager = OldPageManager.getOldPageManager();
-    OldPageController() {}
-    @Get(value = "/{slug}", produces = MediaType.TEXT_JSON)
-    public OldPage list(String slug) {
-        return pageManager.getByPrimaryKey(slug);
-    }
 
-    @Get("/")
-    public String index() {
-        return "OldPage page";
-    }
-
-    @Get(value = "/list", produces = MediaType.TEXT_JSON)
-    public List<OldPage> list() {
-        return pageManager.getAllPages();
-    }
-
-//    @Delete("/")
-//    public HttpResponse deleteAll() {
-//        pageManager.deleteAll();
-//        return HttpResponse.noContent();
-//    }
-    @Post("/")
-    public HttpResponse<OldPage> add(@Body OldPage oldPageToAdd) {
-        OldPage oldPage = pageManager.addOldPage(oldPageToAdd.getPrimaryKey(), oldPageToAdd.getIndex(), oldPageToAdd.getTitle(), oldPageToAdd.getContent());
-        //TODO will still return created even if there's an unsuccessful creation, this if statement prevents that.
-        if (pageManager.getByPrimaryKey(oldPageToAdd.getPrimaryKey()) == null) return HttpResponse.serverError(); //I.e. object didn't get created
-        return HttpResponse
-                .created(oldPage)
-                .headers(headers -> headers.location((location(oldPage.getPrimaryKey()))));
-    }
-
-    @Put("/")
-    public HttpResponse update(@Body OldPage updatedOldPage) {
-        OldPage oldPage = new OldPage(updatedOldPage.getPrimaryKey(), updatedOldPage.getIndex(), updatedOldPage.getTitle(), updatedOldPage.getContent());
-        pageManager.update(oldPage);
-        return HttpResponse
-                .noContent()
-                .header(HttpHeaders.LOCATION, location(updatedOldPage.getPrimaryKey()).getPath());
-    }
-
-    @Delete("/{slug}")
-    public HttpResponse delete(String slug) {
-        pageManager.delete(slug);
-        return HttpResponse.noContent();
-    }
-
-    protected URI location(String slug) { //Make it take not the ID to assign the location
-        String encodedSlug = null;
-        try {
-            encodedSlug = URLEncoder.encode(slug, java.nio.charset.StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException e) {
-            return null;
-        }
-        return URI.create("/page/" + encodedSlug);
-    }
 }
--- back-end/src/main/java/com/projectBackEnd/Entities/Page/PageManager.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Page/PageManager.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 /**
  * PageManager defines methods to interact with the Page table in the database.
  * This class extends the EntityManager.
- *
+ * //TODO Remove methods only used by testing.
  * https://examples.javacodegeeks.com/enterprise-java/hibernate/hibernate-annotations-example/
  */
 public class PageManager extends EntityManager implements PageManagerInterface {
@@ -95,15 +95,15 @@ public class PageManager extends EntityManager implements PageManagerInterface {
      * @param site
      * @return List of pages
      */
-    public List<Page> getAllPagesOfSite(Site site) { return getAllPagesOfSite(site.getName()); }
+    public List<Page> getAllPagesOfSite(Site site) { return getAllPagesOfSite(site.getSlug()); }
 
     /**
      * Get all pages belonging to input site in database
      * @param siteName
      * @return List of pages
      */
-    public List<Page> getAllPagesOfSite(String siteName) {
-        return getAllPages().stream().filter(p -> p.getSite().getName().equals(siteName)).sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
+    public List<Page> getAllPagesOfSite(String siteSlug) {
+        return getAllPages().stream().filter(p -> p.getSite().getSlug().equals(siteSlug)).sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
     }
 
     /**
@@ -113,7 +113,7 @@ public class PageManager extends EntityManager implements PageManagerInterface {
      * @return found Page
      */
     public Page getPageBySiteAndSlug(Site site, String slug) {
-        return getPageBySiteAndSlug(site.getName(), slug);
+        return getPageBySiteAndSlug(site.getSlug(), slug);
     }
 
     /**
