ccc97343cda36bbda82e2ef9728912b78f853ad7
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-03-19 18:12:26 +0000

    Add bulletin board and news card components

---
+++ front-end/seg-major-frontend/src/components/BulletinBoard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card bulletin-board">
+    <div class="header">
+      <p class="title is-4">Bulletin Board</p>
+      <p class="subtitle">Read our latest news and guidance</p>
+    </div>
+
+    <router-link v-for="item in displayItems" :key="item.slug" :to="`/news/${item.slug}`">
+      <news-card :newsItem="item"></news-card>
+    </router-link>
+
+    <router-link to="/news">
+      <button class="button is-fullwidth is-light is-medium">View all</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import NewsCard from '@/components/NewsCard.vue';
+
+export default {
+  name: "BulletinBoard",
+  components: {
+    NewsCard
+  },
+  data () {
+    return {
+      items: []
+    }
+  },
+  computed: {
+    displayItems() {
+      return this.items.slice(0, 5);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bulletin-board {
+  width: 500px;
+  border-top: 5px solid #353535;
+
+  .header {
+    padding: 2rem 1.5rem;
+  }
+
+  .no-news-message {
+    height: 50px;
+  }
+}
+</style>
