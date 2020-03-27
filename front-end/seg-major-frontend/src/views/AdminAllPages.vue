f0b9f0bdb15c9dd789d785df1ad2f7df3db56d31
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-27 17:17:35 +0000

    Show empty state text when there are no pages

--- front-end/seg-major-frontend/src/views/AdminAllPages.vue
+++ front-end/seg-major-frontend/src/views/AdminAllPages.vue
@@ -7,6 +7,12 @@
         <transition name="fade" mode="out-in">
           <loading-spinner v-if="!pages" style="margin-top: 50px;"></loading-spinner>
           <div v-else class="pages-list">
+            <div v-if="pages.length == 0" class="notification has-text-centered">
+              <h3 class="title is-5">There are no pages to show here.</h3>
+              <p
+                class="subtitle is-6"
+              >You can create a new page using the add button in the bottom left!</p>
+            </div>
             <router-link
               v-bind:to="page.slug"
               append
