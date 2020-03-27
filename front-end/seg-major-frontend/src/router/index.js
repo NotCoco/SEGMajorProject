e46a12f0fe95cdea02def41b926ab0a024b7dea1
Andy <andrea.ciuffo@kcl.ac.uk>
2020-03-27 20:04:44 +0000

    Create SearchResults view

--- front-end/seg-major-frontend/src/router/index.js
+++ front-end/seg-major-frontend/src/router/index.js
@@ -14,6 +14,7 @@ import AdminEditPage from '../views/AdminEditPage.vue'
 import SiteContentViewerLayout from '../views/SiteContentViewerLayout.vue'
 import PageViewer from '../views/PageViewer.vue'
 import AdminSiteSettings from '../views/AdminSiteSettings.vue'
+import SearchResults from '../views/SearchResults.vue'
 
 Vue.use(VueRouter)
 
@@ -80,6 +81,10 @@ const routes = [
     path: '/:siteSlug',
     component: SiteContentViewerLayout,
     children: [
+      {
+        path: 'search',
+        component: SearchResults
+      },
       {
         path: ':pageSlug',
         component: PageViewer
