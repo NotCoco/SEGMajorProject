43766e5a356bddec15b73dfdf08540982826efaf
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-03-30 02:22:50 +0100

    Create regex from search query

--- front-end/seg-major-frontend/src/services/search-service.js
+++ front-end/seg-major-frontend/src/services/search-service.js
@@ -1,4 +1,10 @@
+function getSearchRegex(query) {
+  const escapedQuery = query.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+  return new RegExp(escapedQuery, 'i');
+}
+
 export default {
   search(pages, query) {
+    const regex = getSearchRegex(query);
   }
 }
