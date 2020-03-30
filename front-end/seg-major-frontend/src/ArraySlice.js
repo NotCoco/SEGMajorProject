7bf8a64eb407aabcd3951384ecdddd1f2e671808
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-03-30 04:59:19 +0100

    Document ArraySlice with JSDoc

--- front-end/seg-major-frontend/src/ArraySlice.js
+++ front-end/seg-major-frontend/src/ArraySlice.js
@@ -1,8 +1,34 @@
+/**
+ * Represents a slice of an array by reference.
+ * Use to avoid copying elements and creating new arrays.
+ */
 export default class ArraySlice {
+  /**
+   * The array that has been sliced into.
+   * 
+   * @type {*[]}
+   */
   #array;
+  /**
+   * The beginning of the specified portion of the array.
+   * 
+   * @type {number}
+   */
   #from;
+  /**
+   * The length of the slice.
+   * 
+   * @type {number}
+   */
   #length;
 
+  /**
+   * Creates an instance of ArraySlice.
+   * 
+   * @param {*[]} array The array to slice into
+   * @param {number} from The beginning of the specified portion of the array
+   * @param {number} maxLength The maximum length of the slice
+   */
   constructor(array, from, maxLength) {
     if (!(array instanceof Array)) throw new Error("array must be array");
     if (maxLength < 0) throw new Error("maxLength cannot be negative");
@@ -17,15 +43,32 @@ export default class ArraySlice {
     }
   }
 
+  /**
+   * Accesses the item in the array slice at the given index.
+   *
+   * @param {number} i The index to access
+   * @returns The value at the given index in the array slice
+   */
   get(i) {
     if (i < 0 || i >= this.length) throw new Error("Index out of bounds");
     return this.#array[i + this.#from];
   }
 
+  /**
+   * Gets the length of the array slice.
+   *
+   * @type {number} 
+   * @readonly
+   */
   get length() {
     return this.#length;
   }
 
+  /**
+   * Gets an iterable for the array slice.
+   *
+   * @returns {Iterable.<*>} An iterable over this array slice
+   */
   [Symbol.iterator]() {
     let index = this.#from - 1;
     const lastIndex = index + this.length;
