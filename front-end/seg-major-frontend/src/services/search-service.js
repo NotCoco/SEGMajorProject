bb5b7175bbdd9a17d27e96cd307d2eda279df28b
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-03-30 02:25:06 +0100

    Define basic search flow

--- front-end/seg-major-frontend/src/services/search-service.js
+++ front-end/seg-major-frontend/src/services/search-service.js
@@ -3,8 +3,24 @@ function getSearchRegex(query) {
   return new RegExp(escapedQuery, 'i');
 }
 
+function searchPage(page, regex) {
+  if (page.content) {
+    let weight = searchNode(JSON.parse(page.content), regex);
+    if (regex.test(page.title)) weight += 7;
+    return weight;
+  }
+  else return 0;
+}
+
+function searchNode(node, regex) {
+}
+
 export default {
   search(pages, query) {
     const regex = getSearchRegex(query);
+    return pages.map(page => ({
+                      page: page,
+                      weight: searchPage(page, regex)
+                     }))
   }
 }
