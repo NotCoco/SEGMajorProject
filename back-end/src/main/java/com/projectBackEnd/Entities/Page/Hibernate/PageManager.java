bab2e068b69f564f682ba0ecda36ba305e93330f
k1888763 <jeanne.beyazian@kcl.ac.uk>
2020-04-02 16:00:19 +0100

    Comment Page package

--- back-end/src/main/java/com/projectBackEnd/Entities/Page/Hibernate/PageManager.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Page/Hibernate/PageManager.java
@@ -3,10 +3,11 @@ package main.java.com.projectBackEnd.Entities.Page.Hibernate;
 import main.java.com.projectBackEnd.Entities.Site.Hibernate.Site;
 import main.java.com.projectBackEnd.EntityManager;
 import main.java.com.projectBackEnd.HibernateUtility;
-import javax.persistence.PersistenceException;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 /**
  * PageManager defines methods to interact with the Page table in the database.
@@ -17,14 +18,19 @@ public class PageManager extends EntityManager implements PageManagerInterface {
 
     private static PageManagerInterface pageManager;
 
+    /**
+     * Private constructor (Singleton design pattern)
+     */
     private PageManager() {
+
         super();
         setSubclass(Page.class);
         HibernateUtility.addAnnotation(Site.class);
         HibernateUtility.addAnnotation(Page.class);
-
         pageManager = this;
+
     }
+
 
     /**
      * Get page manager interface
@@ -35,61 +41,64 @@ public class PageManager extends EntityManager implements PageManagerInterface {
         else return new PageManager();
     }
 
+
     /**
-     * Insert input Page object into the database
-     * @param newPage
+     * Insert a Page object into the database
+     * @param newPage   Page to add to the database
      * @return added object
      */
     public Page addPage(Page newPage) {
-        if (getPageBySiteAndSlug(newPage.getSite().getSlug(), newPage.getSlug()) != null) throw new PersistenceException();
         return (Page) super.insertTuple(newPage);
     }
 
+
     /**
-     * Find Page object associated to input primary key in the database
-     * @param pk
+     * Update attributes of the given Page object
+     * @return Page object with updated attributes
+     */
+    public Page update(Page updatedVersion) { super.update(updatedVersion); return updatedVersion; }
+
+
+    /**
+     * Retrieve Page object corresponding to the given primary key from the database
+     * @param pk    Primary key of the Page to find
      * @return found Page
      */
     public Page getByPrimaryKey(Integer pk) {
         return (Page) super.getByPrimaryKey(pk);
     }
 
+
     /**
-     * Get all objects from Page table stored in the database
+     * Get all Pages belonging to the given site from the database
+     * @param siteSlug  Slug of the parent site
+     * @return List of pages belonging to parent site
+     */
+    public List<Page> getAllPagesOfSite(String siteSlug) {
+        return getAllPages().stream().filter(p -> p.getSite().getSlug().equals(siteSlug))
+                .sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
+    }
+
+
+    /**
+     * Retrieve Page associated to input site and slug in the database
+     * @param siteSlug  Slug of the parent Site
+     * @param slug      Slug of the Page
+     * @return found Page
+     */
+    public Page getPageBySiteAndSlug(String siteSlug, String slug) {
+        List<Page> matches = getAllPages().stream().filter(p -> p.getSite().getSlug().equals(siteSlug) && p.getSlug().equals(slug))
+                .sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
+        return matches.size()>= 1 ? matches.get(0) : null;
+    }
+
+
+    /**
+     * Get all the objects from the Page table stored in the database
      * @return List of all pages in Page table
      */
     public List<Page> getAllPages() {
         return (List<Page>) super.getAll();
-    }
-
-    /**
-     * Get all pages belonging to input site in database
-     * @param siteSlug
-     * @return List of pages
-     */
-    public List<Page> getAllPagesOfSite(String siteSlug) {
-        return getAllPages().stream().filter(p -> p.getSite().getSlug().equals(siteSlug)).sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
-    }
-
-    /**
-     * Find Page associated to input site and slug in the database
-     * @param siteSlug
-     * @param slug
-     * @return found Page
-     */
-    public Page getPageBySiteAndSlug(String siteSlug, String slug) {
-        List<Page> matches = getAllPages().stream().filter(p -> p.getSite().getSlug().equals(siteSlug) && p.getSlug().equals(slug)).sorted(Comparator.comparingInt(Page::getIndex)).collect(Collectors.toList());
-        return matches.size()>= 1 ? matches.get(0) : null;
-    }
-
-    /**
-     * Update attributes of the object
-     * @return updated object
-     */
-    public Page update(Page updatedVersion) {
-        Page pageMatch = getPageBySiteAndSlug(updatedVersion.getSite().getSlug(), updatedVersion.getSlug());
-        if (pageMatch != null && !pageMatch.getPrimaryKey().equals(updatedVersion.getPrimaryKey())) throw new PersistenceException();
-        super.update(updatedVersion); return updatedVersion;
     }
 
 
--- back-end/src/main/java/com/projectBackEnd/Entities/Page/Micronaut/PagePatchCommand.java
+++ back-end/src/main/java/com/projectBackEnd/Entities/Page/Micronaut/PagePatchCommand.java
@@ -5,49 +5,100 @@ import javax.validation.constraints.NotNull;
 //TODO Reinsert tags as I couldn't get them to compile on IntelliJ Jars.
 
 
+/**
+ * PagePatchCommand is used by its controller to patch a page object in the database
+ * It is an implementation of the Command design pattern.
+ * It creates mock Page objects and reduce memory use.
+ */
 //@Introspected
 public class PagePatchCommand {
 
+    // Primary key ID of the page
     @NotNull
     private int id;
 
+    // Slug of the page for identification
     @NotBlank
     private String slug;
 
+    // Index of the page : its position on the site
     @NotNull
     private int index;
 
-    public PagePatchCommand() {
 
-    }
+    /**
+     * Default constructor
+     */
+    public PagePatchCommand() {}
 
+
+    /**
+     * Main constructor
+     * @param id    Primary key 'ID' of the Page
+     * @param slug  Slug of the Page
+     * @param index Index of the Page
+     */
     public PagePatchCommand(int id, String slug, int index) {
+
         this.id = id;
         this.slug = slug;
         this.index = index;
+
     }
 
-    public void setSlug(String slug){
-        this.slug = slug;
-    }
 
+    /**
+     * Set the ID of that page as the input ID
+     * @param id    Primary key 'ID'
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getIndex() {
-        return index;
-    }
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
+    /**
+     * Get primary key ID for this object
+     * @return primary key
+     */
     public int getId() {
         return id;
     }
 
+
+    /**
+     * Set the index of the Page as the input index value
+     * @param index New index value
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+
+    /**
+     * Get the index of the Page
+     * @return index
+     */
+    public Integer getIndex() {
+        return index;
+    }
+
+
+    /**
+     * Set the slug of the Page to the input slug
+     * @param slug New slug value
+     */
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+
+    /**
+     * Get the slug of the Page
+     * @return slug value
+     */
     public String getSlug() {
         return slug;
     }
+
+
 }
