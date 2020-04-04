0980e698c67756196ae001a2cd9cc61332422854
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-04-04 19:52:46 +0100

    Remove references to bootstrap

--- front-end/seg-major-frontend/src/main.js
+++ front-end/seg-major-frontend/src/main.js
@@ -6,17 +6,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCircleNotch, faStream, faBars } from '@fortawesome/free-solid-svg-icons'
 import { faFrown } from '@fortawesome/free-regular-svg-icons'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
 
 Vue.use(Print)
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
 library.add(faCircleNotch, faFrown, faStream, faBars)
 
