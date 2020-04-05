aa23500642913f78964de4002933f3db7f3f7194
Andy <andrea.ciuffo@kcl.ac.uk>
2020-04-05 20:04:46 +0100

    Optimise Admin navbar for mobile devices

--- front-end/seg-major-frontend/src/views/AdminLayout.vue
+++ front-end/seg-major-frontend/src/views/AdminLayout.vue
@@ -5,43 +5,59 @@
         <div class="navbar-brand">
           <router-link to="/admin" class="navbar-item text-brand">Admin</router-link>
 
-          <router-link to="/admin/sites" class="navbar-item" active-class="is-active">Sites</router-link>
-          <router-link
-            to="/admin/drug-chart"
-            class="navbar-item"
-            active-class="is-active"
-          >Drug Chart</router-link>
-
-          <a class="navbar-item">News</a>
-          <router-link to="/admin/settings" class="navbar-item" active-class="is-active">Settings</router-link>
+          <div class="navbar-burger is-hidden-desktop" @click="mobileNavActive=!mobileNavActive">
+            <font-awesome-icon icon="bars" size="2x" />
+          </div>
         </div>
 
-        <div class="navbar-menu">
-          <div class="navbar-end">
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                <i class="material-icons" style="margin-right: 12px; opacity: 0.92;">person</i>
-                {{username}}
-              </a>
+        <transition name="fade" mode="out-in">
+          <div
+            class="navbar-menu"
+            :class="{ 'is-active': mobileNavActive }"
+            v-bind:key="mobileNavActive"
+          >
+            <div class="navbar-start">
+              <router-link to="/admin/sites" class="navbar-item" active-class="is-active">Sites</router-link>
+              <router-link
+                to="/admin/drug-chart"
+                class="navbar-item"
+                active-class="is-active"
+              >Drug Chart</router-link>
 
-              <div class="navbar-dropdown is-right">
-                <router-link to="/admin/settings#your-account" class="navbar-item">Your account</router-link>
-                <router-link to="/admin/settings#change-email" class="navbar-item">Change Email</router-link>
-                <router-link
-                  to="/admin/settings#change-password"
-                  class="navbar-item"
-                >Change Password</router-link>
-                <hr class="navbar-divider" />
-                <router-link
-                  to="/admin/settings/create-new-user"
-                  class="navbar-item"
-                >Create new user</router-link>
-                <hr class="navbar-divider" />
-                <a class="navbar-item log-out" @click="logout()">Log out</a>
+              <a class="navbar-item">News</a>
+              <router-link
+                to="/admin/settings"
+                class="navbar-item"
+                active-class="is-active"
+              >Settings</router-link>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item has-dropdown is-hoverable is-hidden-touch">
+                <a class="navbar-link">
+                  <i class="material-icons" style="margin-right: 12px; opacity: 0.92;">person</i>
+                  {{username}}
+                </a>
+
+                <div class="navbar-dropdown is-right">
+                  <router-link to="/admin/settings#your-account" class="navbar-item">Your account</router-link>
+                  <router-link to="/admin/settings#change-email" class="navbar-item">Change Email</router-link>
+                  <router-link
+                    to="/admin/settings#change-password"
+                    class="navbar-item"
+                  >Change Password</router-link>
+                  <hr class="navbar-divider" />
+                  <router-link
+                    to="/admin/settings/create-new-user"
+                    class="navbar-item"
+                  >Create new user</router-link>
+                  <hr class="navbar-divider" />
+                  <a class="navbar-item log-out" @click="logout()">Log out</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </transition>
       </div>
     </nav>
 
@@ -59,7 +75,8 @@ import UserService from "@/services/user-service";
 export default {
   data() {
     return {
-      username: "..."
+      username: "...",
+      mobileNavActive: false
     };
   },
   methods: {
@@ -91,5 +108,15 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+}
+
+.navbar {
+  height: 55px;
+  
+  .navbar-burger {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
