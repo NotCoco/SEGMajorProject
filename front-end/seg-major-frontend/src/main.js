367c6700de8cd3c87221d482a696c9c983a90c43
Andy <andrea.ciuffo@kcl.ac.uk>
2020-04-05 13:02:58 +0100

    Implement saving state and saved notification

--- front-end/seg-major-frontend/src/main.js
+++ front-end/seg-major-frontend/src/main.js
@@ -4,10 +4,10 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCircleNotch, faStream, faBars } from '@fortawesome/free-solid-svg-icons'
-import { faFrown } from '@fortawesome/free-regular-svg-icons'
+import { faFrown, faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 import Vuelidate from 'vuelidate'
 
-library.add(faCircleNotch, faFrown, faStream, faBars)
+library.add(faCircleNotch, faFrown, faStream, faBars, faCheckCircle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(Vuelidate)
 
