c868af1a28625797696c051e262983ea904eb1d3
Callum Watkins <callum.watkins@kcl.ac.uk>
2020-04-08 20:30:53 +0100

    Change base API URL to production domain

--- front-end/seg-major-frontend/src/services/backend-service.js
+++ front-end/seg-major-frontend/src/services/backend-service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://getnova.app'
 })
 
 // request interceptor
