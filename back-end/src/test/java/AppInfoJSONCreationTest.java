0f98e69f4cad870ad418462c2ad8e7b3e0fb3c80
Wasif <wasifofo@yahoo.co.uk>
2020-04-10 18:07:18 +0100

    Add JSON file creation test

---
+++ back-end/src/test/java/AppInfoJSONCreationTest.java
@@ -0,0 +1,55 @@
+package test.java;
+
+import main.java.com.projectBackEnd.Services.AppInfo.AppInfo;
+import main.java.com.projectBackEnd.Services.AppInfo.AppInfoManager;
+import main.java.com.projectBackEnd.Services.AppInfo.AppInfoManagerInterface;
+import main.java.com.projectBackEnd.Services.AppInfo.JSONLocation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import java.io.File;
+import static org.junit.Assert.*;
+
+/**
+ * This class tests that a JSON will be created if the file location is not existing already.
+ * It will take place on servers etc. It must be in a separate class as the file is a class static variable.
+ */
+class AppInfoJSONCreationTest {
+
+    private static AppInfoManagerInterface infoManager;
+    private static String path = "HelloIWillExist.json";
+
+    /**
+     * Set the JSON's location and initialise a manager for testing
+     */
+    @BeforeEach
+    void setUp() {
+        JSONLocation.setJsonFile(path);
+        //Setting this will create the file itself before the AppInfoManager is initialised
+        infoManager = AppInfoManager.getInfoManager();
+    }
+
+    @AfterAll
+    static void deleteFile() {
+        File madeFile = new File(path);
+        if (madeFile.exists()) madeFile.delete();
+    }
+
+//======================================================================================================================
+
+    /**
+     * Test setting hospital's information, multiple times to show rewrites
+     */
+    @Test
+    void testUpdateAndGetInformation() {
+        infoManager.updateInfo(new AppInfo("Interesting Hospital", "Cool Department"));
+        assertEquals(infoManager.getInfo().getHospitalName(), "Interesting Hospital");
+    }
+    /**
+     * Prove the created file exists
+     */
+    @Test
+    void testCreatedFileExists() {
+        assertTrue(new File(path).exists());
+    }
+}
